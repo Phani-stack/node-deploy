ac03f2629705cfd5046ac561ec6780ec5669f6bc
Phani-stack <kdlphani916@gmail.com>
2025-10-26 20:45:56 +0530

    v1

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import 'dotenv/config'
+
+const port  = process.env.PORT;
+
+const app = express()
+app.use(express.json());
+
+app.post('/add', (req, res) => {
+   let { a, b } = req.body;
+   a = parseInt(a);
+   b = parseInt(b);
+   const sum = a + b;
+   return res.status(200).json({ sum: sum });
+})
+
+
+app.listen(port, () => {
+   console.log("Running..")
+})
